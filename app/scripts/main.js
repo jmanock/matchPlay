958948c99f4e0c8d01a0f7d2b9f7e2153be3ff7a
Jon Manock <jonmanock@Jons-MacBook-Air.local>
2017-12-27 06:01:36 -0500

    total √

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
   $.getJSON('scripts/leaderboard-v2.json', function(json){
     var players = json.leaderboard.players;
     for(var i = 0; i< players.length; i++){
+      var total = 0;
       var firstName = players[i].player_bio.first_name;
       var lastName = players[i].player_bio.last_name;
       var fullName = firstName +' '+ lastName;
@@ -10,9 +11,10 @@ $(document).ready(function(){
       console.log(fullName);
       for(var x = 0; x<rounds.length; x++){
         var strokes = rounds[x].strokes;
-        console.log(strokes);         
+        console.log(strokes);
+        total += strokes;
       }
-
+      console.log(total);
     }
   });
 });
