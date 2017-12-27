6f3e4b884e147a7ed5ab0a9b01edc1d0d4785daa
Jon Manock <jonmanock@Jons-MacBook-Air.local>
2017-12-27 06:11:46 -0500

    shows up

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,20 +1,23 @@
 $(document).ready(function(){
   $.getJSON('scripts/leaderboard-v2.json', function(json){
     var players = json.leaderboard.players;
+
     for(var i = 0; i< players.length; i++){
       var total = 0;
       var firstName = players[i].player_bio.first_name;
       var lastName = players[i].player_bio.last_name;
       var fullName = firstName +' '+ lastName;
       var rounds = players[i].rounds;
-      // needs to handle the first round
-      console.log(fullName);
+
+      // console.log(fullName);
+      $('.names').append('<li>'+fullName+'</li>');
       for(var x = 0; x<rounds.length; x++){
         var strokes = rounds[x].strokes;
-        console.log(strokes);
+        // console.log(strokes);
         total += strokes;
       }
-      console.log(total);
+      // console.log(total);
+      $('.names').append('<p>'+total+'</p>');
     }
   });
 });
