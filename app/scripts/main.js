102dae2758d29f816e7b37a4ca403e2d26066078
Jon Manock <jonmanock@Jons-MacBook-Air.local>
2017-12-27 05:13:21 -0500

    got list of players

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
-  $.getJSON('scripts/schedule-v2.json', function(json){
-    console.log(json);
+  $.getJSON('scripts/leaderboard-v2.json', function(json){
+    var players = json.leaderboard.players;
+    for(var i = 0; i< players.length; i++){
+      var firstName = players[i].player_bio.first_name;
+      var lastName = players[i].player_bio.last_name;
+      var fullName = firstName +' '+ lastName;
+      
+    }
   });
 });
